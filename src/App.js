1b3efc5f44836e53e99a418387082defe19dd4b9
HelloJon29 <jonkim1996@gmail.com>
2023-08-22 23:59:11 -0500

    Added firebase into project, updated ChatBox component, and App.js to allow for dynamic messages, also updated css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Background from './components/Background';
 import ChatBox from './components/ChatBox';
+import firebase from './firebaseConfig';
+import AuthiModal from './components/AuthiModal';
 
 function App() {
+  // state to update when user is authenticated
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((authUser) => {
+      if (authUser) {
+        setUser(authUser);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
     <div className="App">
       <Background />
-      <ChatBox />
+      {!user ? <AuthiModal /> : <ChatBox currentUser={user} />}
     </div>
   );
 }
--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,21 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from '../firebaseConfig'; // Secrets File
 import '../Styles/ChatBox.css';
 
-const ChatBox = () => {
-  // Hardcode the chat box first to style and get an idea, return basic html
+const ChatBox = ({ currentUser }) => {
+  // use state to update messages
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  // reference to firestore collection
+  const messageRef = firebase.firestore().collection('messages');
+
+  useEffect(() => {
+    // Listen for changes in the firestore collection
+    const unsubscribe = messageRef.onSnapshot((snapshot) => {
+      const messageData = snapshot.docs.map((doc) => doc.data());
+      setMessages(messageData);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  // crreate the new message object to send to the back end here
+  const handleSendMessage = () => {
+    if (newMessage.trim() === '') return;
+
+    // store the info in JSON format
+    const newMessageObj = {
+      text: newMessage,
+      timestamp: new Date().toISOString(),
+      userId: firebase.auth().currentUser.uid,
+    };
+
+    // add the new message to Firestore
+    messageRef.add(newMessageObj);
+
+    setNewMessage('');
+  };
+
   return (
     <div className="chat-box">
       <div className="message-list">
-        <span className="message-text">Hello! World! We Are Live!</span>
-        <span className="message-time">12:41AM</span>
-      </div>
-      <div className="message">
-        <span className="message-text">Wow! That's so cool!</span>
-        <span className="message-time">12:42AM</span>
+        {messages
+          .slice()
+          .reverse()
+          .map((message, index) => (
+            <div
+              key={index}
+              className={`message ${
+                message.userId === currentUser.uid ? 'own-message' : ''
+              }`}
+            >
+              {message.text}
+            </div>
+          ))}
       </div>
       <div className="message-input">
-        <input type="text" placeholder="Type a message..." />
-        <button>Send</button>
+        <input
+          type="text"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          placeholder="Type a message..."
+        />
+        <button onClick={handleSendMessage}>Send</button>
       </div>
     </div>
   );
